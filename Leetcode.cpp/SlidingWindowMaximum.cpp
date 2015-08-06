3840154fd42d1ea88d005de5a15de4874edd86ab
Alex Tsui <alextsui05@gmail.com>
2015-08-06 06:42:48 -0700

    Fix typo

--- Leetcode.cpp/SlidingWindowMaximum.cpp
+++ Leetcode.cpp/SlidingWindowMaximum.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> res;
         if (!nums.size() || k == 0)
             return res;
-            
+
         // index into nums
         // front should always be the max in the current window
         deque<int> candidates;
@@ -14,14 +14,14 @@ public:
                 candidates.pop_front();
             }
             while (candidates.size() && nums[ candidates.back() ] < val) {
-                candidates.pop_back(;)gith
+                candidates.pop_back();
             }
             candidates.push_back(i);
         }
 
         for (int i = 0; i <= nums.size() - k; ++i) {
             res.push_back(nums[candidates.front()]);
-            
+
             // remove expired candidates
             while (candidates.size() &&
                 candidates.front() <= i) {
@@ -35,10 +35,10 @@ public:
             }
             while (candidates.size() && nums[ candidates.back() ] < val) {
                 candidates.pop_back();
-            }            
+            }
             candidates.push_back(i + k);
         }
-        
+
         return res;
     }
 };
